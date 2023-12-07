f1fa1efcaf975b715f027abcbf5af215f2dc74e7
CompanyAccountUserPersonnelOperator <emailcompanyaccount@proton.me>
2023-12-07 00:54:02 +0000

    MORE CHANGES

--- final_database/prepared_statement_mt_state_senators.py
+++ final_database/prepared_statement_mt_state_senators.py
@@ -64,5 +64,58 @@ def updateRep():
     return render_template('rep-update.html', id=id, existingFName=existingFName, existingLName=existingLName, existingDistrict=existingDistrict, existingParty=existingParty)
 
 
+@app.route('/bill-list.html', methods=['GET'])
+def showBills():
+    with open('secrets.json', 'r') as secretsFile:
+        creds = json.load(secretsFile)['mysqlCredentials']
+    connection = mysql.connector.connect(**creds)
+
+    mycursor = connection.cursor()
+    newName = request.args.get('name')
+    newCategory = request.args.get('category')
+    newDate = request.args.get('date')
+    if newName is not None and newCategory is not None and newDate is not None:
+        mycursor.execute("INSERT INTO bills (billname, category, date) values (%s, %s, %s)", (newName, newCategory, newDate))
+        connection.commit()
+    elif request.args.get('delete') == 'true':
+        deleteID = request.args.get('id')
+        mycursor.execute("DELETE FROM bills WHERE billID=%s", (deleteID,))
+        connection.commit()
+
+    mycursor.execute("SELECT billID, billname, category, date FROM bills")
+    myresult = mycursor.fetchall()
+    mycursor.close()
+    connection.close()
+    return render_template('bill-list.html', collection=myresult)
+
+
+@app.route("/updateBill")
+def updateBill():
+    with open('secrets.json', 'r') as secretsFile:
+        creds = json.load(secretsFile)['mysqlCredentials']
+    connection = mysql.connector.connect(**creds)
+
+    id = request.args.get('id')
+    newName = request.args.get('name')
+    newCategory = request.args.get('category')
+    newDate = request.args.get('date')
+    if id is None:
+        return "Error, ID not specified"
+    elif newName is not None and newCategory is not None and newDate is not None:
+        mycursor = connection.cursor()
+        mycursor.execute("UPDATE bills SET billname=%s, category=%s, date=%s WHERE billID=%s", (newName, newCategory, newDate, id))
+        mycursor.close()
+        connection.commit()
+        connection.close()
+        return redirect(url_for('showBills'))
+
+    mycursor = connection.cursor()
+    mycursor.execute("SELECT billID, billname, category, date, FROM bills WHERE billID=%s", (id,))
+    id, existingName, existingCategory, existingDate = mycursor.fetchone()
+    mycursor.close()
+    connection.close()
+    return render_template('bill-update.html', id=id, existingName=existingName, existingCategory=existingCategory, existingDate=existingDate)
+
+
 if __name__ == '__main__':
     app.run(port=8000, host="0.0.0.0")
